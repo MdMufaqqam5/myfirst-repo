e0e20674f599cb47def5a24af0e5ec5a1e80e264
mohammed mufaqqam sanjar ghori <mdmufaqqam5@gmail.com>
2024-05-11 13:21:51 +0530

    Add files via upload

---
+++ fff.py
@@ -0,0 +1,20 @@
+Code = 3
+code = 33
+print(id(Code))
+print(id(code))
+
+CODE = 33
+code = 33
+print(id(CODE))
+print(id(code))
+
+CODE = 3
+code = 3
+print(id(Code))
+print(id(code))
+
+Code = 3
+Code = 33
+print(id(Code))
+print(id(Code))
+print(Code)
